7cbed76d186f9fc3bdc564b3968cc55ab240dbea
석찬 윤 <ysc0731@ajou.ac.kr>
2024-12-09 00:59:31 +0900

    fix: conflict 오류해결

--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -10,18 +10,6 @@ import Button from "../components/Button";
 import { days, colorClasses } from "../constants/schedule";
 import { generateTimeSlots, convertIndexToTime } from "../utils/time";
 
-const colorClasses = [
-  "bg-indigo-300 hover:bg-indigo-400",
-  "bg-purple-300 hover:bg-purple-400",
-  "bg-pink-300 hover:bg-pink-400",
-  "bg-blue-300 hover:bg-blue-400",
-  "bg-green-300 hover:bg-green-400",
-  "bg-yellow-300 hover:bg-yellow-400",
-  "bg-red-300 hover:bg-red-400",
-  "bg-orange-300 hover:bg-orange-400",
-  "bg-teal-300 hover:bg-teal-400",
-  "bg-cyan-300 hover:bg-cyan-400",
-];
 const SchedulePage = () => {
   const timeSlots = useMemo(() => generateTimeSlots(), []);
   const [schedules, setSchedules] = useState([]);
